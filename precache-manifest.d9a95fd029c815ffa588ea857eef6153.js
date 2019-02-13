330b0e2ea0d09909d6ee2a33c75f08623f3ccba5
nVar <nkeledjian@gmail.com>
2019-02-12 17:01:49 -0800

    Updates

---
+++ precache-manifest.d9a95fd029c815ffa588ea857eef6153.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f0724a0a392b4ba4d33",
+    "url": "/memorygame/static/js/runtime~main.1f0724a0.js"
+  },
+  {
+    "revision": "064f27f8f4f32a96a995",
+    "url": "/memorygame/static/js/main.064f27f8.chunk.js"
+  },
+  {
+    "revision": "fd698e5e35354e9c6cfd",
+    "url": "/memorygame/static/js/1.fd698e5e.chunk.js"
+  },
+  {
+    "revision": "064f27f8f4f32a96a995",
+    "url": "/memorygame/static/css/main.beccc6f4.chunk.css"
+  },
+  {
+    "revision": "11d386966ebf77b9afc6ed7978afa250",
+    "url": "/memorygame/index.html"
+  }
+];
